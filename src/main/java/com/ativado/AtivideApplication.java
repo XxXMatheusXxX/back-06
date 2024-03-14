6fa65d557e24e8764389e6c67134d457ed4423aa
XxXMatheusXxX <matheus.physicist369@gmail.com>
2024-03-14 14:21:16 -0300

    tomaai

---
+++ src/main/java/com/ativado/AtivideApplication.java
@@ -0,0 +1,13 @@
+package com.ativado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtivideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtivideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ativado/AtivideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ativado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtivideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
